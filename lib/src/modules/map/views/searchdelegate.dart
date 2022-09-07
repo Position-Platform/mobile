019d998c2e141f6756308d72676c3b25715aafab
BorisGautier <tchoukouahaboris@outlook.fr>
2022-09-07 13:56:13 +0100

    Feature : Build Map Home Page #6

--- lib/src/modules/map/views/searchdelegate.dart
+++ lib/src/modules/map/views/searchdelegate.dart
@@ -22,7 +22,7 @@ class CustomSearchDelegate extends SearchDelegate {
         onPressed: () {
           query = '';
         },
-        icon: Icon(Icons.clear),
+        icon: const Icon(Icons.clear),
       ),
     ];
   }
@@ -34,7 +34,7 @@ class CustomSearchDelegate extends SearchDelegate {
       onPressed: () {
         close(context, null);
       },
-      icon: Icon(Icons.arrow_back),
+      icon: const Icon(Icons.arrow_back),
     );
   }
 
